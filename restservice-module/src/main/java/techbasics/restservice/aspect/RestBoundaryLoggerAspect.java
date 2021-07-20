a1c9b9dcad04d3d385721b61a78ca187686f63fe
Lloyd <lloyd.louw@mmiholdings.co.za>
2021-07-20 15:26:41 +0200

    Aspect improvement

--- restservice-module/src/main/java/techbasics/restservice/aspect/RestBoundaryLoggerAspect.java
+++ restservice-module/src/main/java/techbasics/restservice/aspect/RestBoundaryLoggerAspect.java
@@ -4,12 +4,43 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.util.StringUtils;
 
 @Aspect
 @Slf4j
 public class RestBoundaryLoggerAspect {
-    @Around("execution(public * techbasics.restservice.domain.service.*.*.*(..))")
+
+    //Pointcut definitions
+    @Pointcut("@within(org.springframework.web.bind.annotation.RestController)")
+    public void restController() {//No implementation required
+    }
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    public void getMapping() {//No implementation required
+    }
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.PutMapping)")
+    public void putMapping() {//No implementation required
+    }
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.PostMapping)")
+    public void postMapping() {//No implementation required
+    }
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
+    public void deleteMapping() {//No implementation required
+    }
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.PatchMapping)")
+    public void patchMapping() {//No implementation required
+    }
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.RequestMapping)")
+    public void requestMapping() {//No implementation required
+    }
+
+    @Around("restController() && (getMapping() || putMapping() || postMapping() || deleteMapping() || patchMapping() || requestMapping())")
     public Object logRestApiCall(ProceedingJoinPoint pjp) throws Throwable {
         String argsPattern = "";
         for (int i = 0; i < pjp.getArgs().length; i++) {
